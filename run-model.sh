a82b82c3ba9514d282985647d9ffa5d5916909ca
PjrCodes <25995849+PjrCodes@users.noreply.github.com>
2024-12-09 21:40:34 +0530

    bash script now takes data loader as argument

--- run-model.sh
+++ run-model.sh
@@ -1,4 +1,5 @@
 arg1=$1
+data=${2:-UrbanWinds2DDataModule}
 arg2=$(echo "$arg1" | tr '[:upper:]' '[:lower:]')
-echo "python src/main.py fit --model $arg1  --data UrbanWinds2DDataModule --data.data_dir 'data' --trainer.logger TensorBoardLogger --trainer.logger.save_dir './logs/$(echo $arg2)_logs' --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 100000000 --trainer.log_every_n_steps 1"
-python src/main.py fit --model $arg1  --data UrbanWinds2DDataModule --data.data_dir 'data' --trainer.logger TensorBoardLogger --trainer.logger.save_dir "./logs/$(echo $arg2)_logs" --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 100000000 --trainer.log_every_n_steps 1
+echo "python src/main.py fit --model $arg1  --data $data --data.data_dir 'data' --trainer.logger TensorBoardLogger --trainer.logger.save_dir './logs/$(echo $arg2)_logs' --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 100000000 --trainer.log_every_n_steps 1"
+python src/main.py fit --model $arg1  --data $data --data.data_dir 'data' --trainer.logger TensorBoardLogger --trainer.logger.save_dir "./logs/$(echo $arg2)_logs" --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 100000000 --trainer.log_every_n_steps 1
