97555aff896de00417169c61f1d012df57c41130
PjrCodes <25995849+PjrCodes@users.noreply.github.com>
2024-12-10 08:28:54 +0530

    i think the run model sh was changed again

--- run-model.sh
+++ run-model.sh
@@ -1,5 +1,5 @@
 arg1=$1
 data=${2:-UrbanWinds2DDataModule}
 arg2=$(echo "$arg1" | tr '[:upper:]' '[:lower:]')
-echo "python src/main.py fit --model $arg1  --data $data --data.data_dir 'data' --trainer.logger TensorBoardLogger --trainer.logger.save_dir './logs/$(echo $arg2)_logs' --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 600 --trainer.log_every_n_steps 5"
+echo "python src/main.py fit --model $arg1  --data $data --data.data_dir 'data' --trainer.logger TensorBoardLogger --trainer.logger.save_dir './logs/$(echo $arg2)_logs' --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 600 --trainer.log_every_n_steps 1"
 python src/main.py fit --model $arg1  --data $data --data.data_dir 'data' --trainer.logger TensorBoardLogger --trainer.logger.save_dir "./logs/$(echo $arg2)_logs" --trainer.check_val_every_n_epoch 1 --trainer.max_epochs 600 --trainer.log_every_n_steps 5
